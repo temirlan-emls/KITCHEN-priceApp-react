f298e67e173a45fb74beb449d1d60020bcdae1ca
Temirlan Emls <temirlan.emls@gmail.com>
2023-05-03 19:16:30 +0600

    Some changes

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState } from 'react';
 import {
     Badge,
     Button,
@@ -6,9 +6,9 @@ import {
     Container,
     ListGroup,
     ButtonGroup,
-} from "react-bootstrap";
-import { IProduct } from "../models/product.model";
-import { useSnackbar } from "notistack";
+} from 'react-bootstrap';
+import { IProduct } from '../models/product.model';
+import { useSnackbar } from 'notistack';
 
 interface IProductProps {
     product: IProduct;
@@ -21,9 +21,9 @@ const Product: React.FunctionComponent<IProductProps> = ({
 }: IProductProps) => {
     const [details, setDetails] = useState(false);
     function toCurrencForm(number: number) {
-        return new Intl.NumberFormat("ru-KZ", {
-            style: "currency",
-            currency: "KZT",
+        return new Intl.NumberFormat('ru-KZ', {
+            style: 'currency',
+            currency: 'KZT',
             maximumSignificantDigits: 5,
         }).format(number);
     }
@@ -33,15 +33,15 @@ const Product: React.FunctionComponent<IProductProps> = ({
     let margin;
     if (product.price === undefined) {
         kitchen_price = 0;
-    } else if (product.sourceSite === "PROFTORG") {
+    } else if (product.sourceSite === 'PROFTORG') {
         if (product.price < 100000) {
-            kitchen_price = toCurrencForm(product.price * 1.4);
-            margin = toCurrencForm(product.price * 1.4 - product.price);
+            kitchen_price = toCurrencForm(product.price * 2);
+            margin = toCurrencForm(product.price * 2 - product.price);
         } else {
             kitchen_price = toCurrencForm(product.price * 1.3);
             margin = toCurrencForm(product.price * 1.3 - product.price);
         }
-    } else if (product.sourceSite === "DEDTRADE") {
+    } else if (product.sourceSite === 'DEDTRADE') {
         if (product.price < 100000) {
             kitchen_price = toCurrencForm(product.price * 1.15);
             margin = toCurrencForm(product.price * 1.1 - product.price);
@@ -49,7 +49,7 @@ const Product: React.FunctionComponent<IProductProps> = ({
             kitchen_price = toCurrencForm(product.price * 1.1);
             margin = toCurrencForm(product.price * 1.1 - product.price);
         }
-    } else if (product.sourceSite === "PROKITCHEN") {
+    } else if (product.sourceSite === 'PROKITCHEN') {
         if (product.price < 100000) {
             kitchen_price = toCurrencForm(product.price * 1.15);
             margin = toCurrencForm(product.price * 1.1 - product.price);
@@ -61,17 +61,17 @@ const Product: React.FunctionComponent<IProductProps> = ({
 
     let card_code;
     if (product.title === product.prodCode) {
-        card_code = "";
+        card_code = '';
     } else if (product.title !== product.prodCode) {
         card_code = product.prodCode;
     }
 
     const countWrapStyle = {
-        borderTop: "1px solid rgb(255,193,7)",
-        borderBottom: "1px solid rgb(255,193,7)",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
+        borderTop: '1px solid rgb(255,193,7)',
+        borderBottom: '1px solid rgb(255,193,7)',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
     };
 
     const [counterNum, setCounterNum] = useState(1);
@@ -96,41 +96,41 @@ const Product: React.FunctionComponent<IProductProps> = ({
 
     const handleClick = () => {
         enqueueSnackbar(`Добавлено в корзину ${counterNum} шт.`, {
-            variant: "success",
+            variant: 'success',
             autoHideDuration: 1000,
         });
     };
 
     return (
-        <Card style={{ maxWidth: "18rem", minWidth: "8rem" }} className="mt-5">
-            {" "}
+        <Card style={{ maxWidth: '18rem', minWidth: '8rem' }} className="mt-5">
+            {' '}
             <Card.Img
                 variant="top"
                 src={product.imgLink}
-                style={{ maxHeight: "286px" }}
+                style={{ maxHeight: '286px' }}
             />
             <Card.Body>
                 <h6>
                     <Badge bg="secondary" className="w-100">
-                        LAST UPDATE:{" "}
-                        {product.date.replace(".", "/").replace(".", "/")}
+                        LAST UPDATE:{' '}
+                        {product.date.replace('.', '/').replace('.', '/')}
                     </Badge>
                 </h6>
 
                 <Card.Title>{product.title}</Card.Title>
                 <p>{card_code}</p>
                 <Button
-                    variant={details ? "outline-secondary" : "outline-danger"}
+                    variant={details ? 'outline-secondary' : 'outline-danger'}
                     className="w-100 my-2"
                     size="sm"
                     onClick={() => setDetails(!details)}
                 >
-                    {details ? "Меньше" : "Больше"}
+                    {details ? 'Меньше' : 'Больше'}
                 </Button>
                 {details && (
                     <Container>
                         <Card.Text>
-                            {product.desc.replace(/([А-Я])/g, "\n $1").trim()}
+                            {product.desc.replace(/([А-Я])/g, '\n $1').trim()}
                         </Card.Text>
                     </Container>
                 )}
@@ -176,7 +176,7 @@ const Product: React.FunctionComponent<IProductProps> = ({
                     </Button>
                     <Button
                         variant="warning fw-bold"
-                        style={{ color: "black" }}
+                        style={{ color: 'black' }}
                         className="w-100"
                         size="sm"
                         onClick={() => {
